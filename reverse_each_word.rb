18fbcef9ad5c7a50f6e0ea2f8e06bbf75483ee9a
Jane Justiz <janejustiz@gmail.com>
2018-06-15 04:11:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   array = string.split()
+  new_array=[]
   array.collect do |x|
-    x.reverse
+    new_array = x.reverse
   end
-  array.join(" ")
+  new_array.join(" ")
 end
